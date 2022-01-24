ac5a3ad39cf2a775289867c81cd001285e58515a
Ran Ribenzaft <ranrib@gmail.com>
2022-01-24 14:06:09 +0200

    add uninstall form

--- src/background.js
+++ src/background.js
@@ -1,3 +1,7 @@
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({ skipIntro: true, skipRecap: true, skipNext: true });
 });
+
+if (chrome.runtime) {
+  chrome.runtime.setUninstallURL("https://docs.google.com/forms/d/e/1FAIpQLSdu75DIgQwiEYud7I8TgsAkyoUnFyLXRDAnELcN_QIeLGvh5w/viewform");
+}
